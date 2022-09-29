354fe583a06f12cd8dd2fc52cbf96b0a7cf60d04
Felipe <f.g.diaz.92@gmail.com>
2022-09-29 18:46:28 -0300

    Laravel Socialite

--- config/services.php
+++ config/services.php
@@ -29,5 +29,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_OAUTH_ID'),
+        'client_secret' => env('GOOGLE_OAUTH_KEY'),
+        'redirect' => '/google-callback',
+    ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PruebaController;
 use App\Http\Controllers\TipoCalificacionController;
-
+use Laravel\Socialite\Facades\Socialite;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,3 +23,15 @@ Route::get('/tipo-calificacion/{id}',[TipoCalificacionController::class, 'show']
 Route::get('/prueba/{id}',[PruebaController::class, 'show']);
 Route::post('/prueba',[PruebaController::class, 'create']);
 Route::delete('/prueba/{id}',[PruebaController::class, 'destroy']);
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/login', function () {
+        return Socialite::driver('google')->redirect();
+    });
+    Route::get('/google-callback', function () {
+        $user = Socialite::driver('google')->user();
+
+        dd ($user);
+    });
+});
